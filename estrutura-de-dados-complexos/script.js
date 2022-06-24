19e295b66d91c997eef02e78e594af94b18b04e0
Rafael-Arruda <rafaelsilvaarruda72@gmail.com>
2022-06-24 20:41:06 -0300

    Exercício estrutura de dados complexos

---
+++ estrutura-de-dados-complexos/script.js
@@ -0,0 +1,24 @@
+let nomes = ["Maria", "João", "Carlos", "Pedro", "Ana"]
+console.log(nomes);
+
+nomes.unshift("Carolina")
+console.log(nomes);
+
+nomes.pop()
+console.log(nomes);
+
+nomes.push("Fabiana", "Thaissa");
+console.log(nomes);
+
+nomes.shift()
+console.log(nomes);
+
+let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+console.log(numbers);
+
+numbers.sort(function(a, b){
+    return a - b;
+});
+
+console.log(numbers);
+
